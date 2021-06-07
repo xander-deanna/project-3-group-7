93f0f622ff228b0e8e00031e99030ef03581d2f7
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-06-07 16:39:59 -0700

    loging & register user & mongoDB

--- server.js
+++ server.js
@@ -1,27 +1,36 @@
 const express = require("express");
-
-// const cors = require('cors')
-// ^^ do we need?
-
 const mongoose = require("mongoose");
-const routes = require("./routes");
+const bodyParser = require('body-parser');
+const passport = require("passport");
+const users = require("./routes/api/users");
 const app = express();
-const PORT = process.env.PORT || 3001;
 
-// Define middleware here
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-// Serve up static assets (usually on heroku)
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
-}
-// Add routes, both API and view
-app.use(routes);
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false
+  })
+);
+app.use(bodyParser.json());
 
-// Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/meseume");
+// DB Config
+const db = require("./config/keys").mongoURI;
 
-// Start the API server
-app.listen(PORT, function() {
-  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch(err => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+// Routes
+app.use("/api/users", users);
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server up and running on port ${port} !`));
