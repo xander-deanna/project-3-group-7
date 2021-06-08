7c32ffe7014633caa859af7c581ebb9d057f63b7
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-06-07 19:50:54 -0500

    explore.js

---
+++ client/src/components/Search/explore.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Explore extends Component {
+    render() {
+        return (
+            <div>
+                <h3>Explore</h3>
+                <label>Search Artist/search paiting</label>
+                <input></input>
+            </div>
+        );
+    }
+}
+
+//api call 
+//have star option to save
+//database componenet
+//card render, for each search show the paiting in multiple cards(title, pic, etc)
