90905a7bea60f7e6250e99ae3e015f1511c6ce15
Manorath Naphaphone <Naphaphonem@outlook.com>
2021-06-09 16:32:09 -0700

    logout fixed

--- client/src/components/Navigation/Navigation.js
+++ client/src/components/Navigation/Navigation.js
@@ -3,49 +3,72 @@ import { Link } from "react-router-dom";
 import { Navbar } from 'react-bulma-components';
 // import '../../App.css'
 
+import PropTypes from "prop-types";
+import { connect } from "react-redux";
+import { logoutUser } from "../../actions/authActions";
+
 class Navigation extends Component {
+
+  onLogoutClick = e => {
+    e.preventDefault();
+    this.props.logoutUser();
+  };
+
   render() {
     return (
       <Navbar>
         <Navbar.Brand>
           <Navbar.Item>
             <Link className="nav-link logo" to={"/about"}><strong>(<strong className="logo-blue">ME</strong>)<strong>seum</strong></strong></Link>
-            
+
           </Navbar.Item>
-          <Navbar.Burger/>
+          <Navbar.Burger />
         </Navbar.Brand>
-      <Navbar.Menu>
+        <Navbar.Menu>
 
-        {/* very hacky way of getting navbar items centered, please don't delete. */}
-        <Navbar.Container/>
-        <Navbar.Container/>
-        {/* -- end weird hackyness -- */}
+          {/* very hacky way of getting navbar items centered, please don't delete. */}
+          <Navbar.Container />
+          <Navbar.Container />
+          {/* -- end weird hackyness -- */}
 
-        <Navbar.Container>
-          <Navbar.Item>
-            <Link className="nav-link" to={"/about"}><strong>ABOUT</strong></Link>
-          </Navbar.Item>
-          <Navbar.Item>
-          <Link className="nav-link" to={"/explore"}><strong>EXPLORE</strong></Link>
-          </Navbar.Item>
-          <Navbar.Item>
-          <Link className="nav-link" to={"/gallery"}><strong>GALLERY</strong></Link>
-          </Navbar.Item>
-        </Navbar.Container>
-        <div class="navbar-end">
-          <div class="navbar-item">
-            <div class="buttons">
-              <a class="button is-light" onClick={this.onLogoutClick}>
-                <strong>LOGOUT</strong>
-              </a>
+          <Navbar.Container>
+            <Navbar.Item>
+              <Link className="nav-link" to={"/about"}><strong>ABOUT</strong></Link>
+            </Navbar.Item>
+            <Navbar.Item>
+              <Link className="nav-link" to={"/explore"}><strong>EXPLORE</strong></Link>
+            </Navbar.Item>
+            <Navbar.Item>
+              <Link className="nav-link" to={"/gallery"}><strong>GALLERY</strong></Link>
+            </Navbar.Item>
+          </Navbar.Container>
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <button
+                  onClick={this.onLogoutClick}
+                  className="btn btn-large waves-effect waves-light hoverable blue accent-3">
+                  Logout
+            </button>
+              </div>
             </div>
           </div>
-        </div>
-      </Navbar.Menu>
-    </Navbar>
+        </Navbar.Menu>
+      </Navbar>
     );
   }
 }
 
-export default Navigation;
+Navigation.propTypes = {
+  logoutUser: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired
+};
 
+const mapStateToProps = state => ({
+  auth: state.auth
+});
+
+export default connect(
+  mapStateToProps,
+  { logoutUser }
+)(Navigation);
