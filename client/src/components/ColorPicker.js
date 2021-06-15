3e53a791174c384deb36ec5c0ca31d2eb7f42b8e
Deanna <xander.deanna@gmail.com>
2021-06-15 00:30:32 -0500

    updated props for color picker

--- client/src/components/ColorPicker.js
+++ client/src/components/ColorPicker.js
@@ -2,22 +2,19 @@ import React, {useState} from "react";
 import { SketchPicker } from "react-color"
 import Draggable from "react-draggable";
 
-export default () => {
+export default (props) => {
     const [color, setColor] = useState('#ffffff')
     return (
         <Draggable>
             <div>
             <SketchPicker
                 color={ color }
-                onChangeComplete={ (color) => {setColor(color.hex)}}
+                onChangeComplete={ (color) => {
+                    setColor(color.hex)
+                    props.setBackground(color.hex)
+                }}
             />
             <br/>
-            <div style ={{
-                backgroundColor: color,
-                height: '300px',
-                transition: 'ease all 500ms'
-            }}>
-            </div>
             </div>
         </Draggable>
     );
--- client/src/pages/Gallery/Gallery.js
+++ client/src/pages/Gallery/Gallery.js
@@ -7,30 +7,39 @@ import Meseum from "../../utils/meseum-api";
 export default class Gallery extends Component {
     constructor(props) {
         super(props) 
-        this.updateStateCallback = this.updateStateCallback.bind(this)
+        this.deleteArtByID = this.deleteArtByID.bind(this)
+        this.setBackground = this.setBackground.bind(this)
     }
-    state = {artworks: []}
+    state = {
+        artworks: [],
+        color: "#ffffff"
+    }
     componentDidMount () {
         Meseum.getAllArt().then(res => {
             this.setState({artworks: res.data.data})
         })
     }
-    updateStateCallback (_id) {
+    deleteArtByID (_id) {
         this.setState({artworks:this.state.artworks.filter((art) => art._id !== _id)})
+    }
+    setBackground (color) {
+        this.setState({color})
     }
     render() {
         // Attempt at added a color picker and setting default state to white
         // const [color, setColor] = useState('#fff')
 
         return (
-            <>
+            <div style ={{
+                backgroundColor: this.state.color,
+            }}>
                 <Navigation />
                 <section className="section">
                     <div className="container gallery-container">
                         {this.state.artworks.map((value, index) => {
                             return (
                                 <div className="columns is-multiline">
-                                    <GalleryCard {...value} key={index} updateState={this.updateStateCallback}/>
+                                    <GalleryCard {...value} key={index} updateState={this.deleteArtByID}/>
                                 </div>
                             )
                         })}
@@ -39,10 +48,11 @@ export default class Gallery extends Component {
                                 height: "100px",
                                 width: "100px"
                             }}
+                            setBackground={this.setBackground}
                         />
                     </div>
                 </section>
-            </>
+            </div>
         )
     }
 };
