0d8af97526c4cab72ebec8c29b3fdd44c440c3b6
Kayleigh Hanna <kayleighchanna@gmail.com>
2021-06-15 22:41:40 -0500

    styling to gallery delete

--- client/src/pages/Gallery/Gallery.js
+++ client/src/pages/Gallery/Gallery.js
@@ -31,6 +31,7 @@ export default class Gallery extends Component {
 
         return (
             <div style ={{
+                height: '100vh',
                 backgroundColor: this.state.color,
             }}>
                 <Navigation />
